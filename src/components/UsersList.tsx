86baf88f83c987fc444cd519ac2f23cd5127a225
Jaden <jenzbr@gmail.com>
2023-05-04 17:46:03 -0500

    Made logical check for each UserView that checks if a given user is friends with the current logged in user

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { api } from "~/utils/api";
 
@@ -7,8 +8,29 @@ type user = {
   profileImageUrl: string;
 };
 const UserView = (props: user) => {
-  const { fullName, profileImageUrl } = props;
+  const { fullName, profileImageUrl, id } = props;
 
+  const user = useUser().user;
+
+  if (id === user?.id) return null;
+
+  const { data, isLoading } = api.relations.getAll.useQuery();
+  console.log("data is: ", data);
+  console.log("userID is : ", user?.id);
+
+  if (!data) return null;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const relationsInvolvingUser = data?.filter(
+    (relation) => relation.relatingUser.id === user?.id
+  );
+
+  console.log("relationsInvolvingUser: ", relationsInvolvingUser);
+
+  const userIsFriendsWith = relationsInvolvingUser?.filter(
+    (relation) => relation.relatedUser === id
+  );
   return (
     <div className="flex items-center justify-center">
       <Image
@@ -18,17 +40,19 @@ const UserView = (props: user) => {
         height={36}
         width={36}
       />
-      <div>{fullName}</div>
-      {/* Implement check for if users are already friends, make userRelations procedure for 
-      sending a friend request */}
-      <button>Friend Request</button>
+      <div className="mx-2">{fullName}</div>
+      {userIsFriendsWith.length > 0 && (
+        <div className="text-sm">Already friends!</div>
+      )}
+      {userIsFriendsWith.length === 0 && (
+        <button className="text-sm">Friend Request</button>
+      )}
     </div>
   );
 };
 
 export const UsersList = () => {
   const { data, isLoading } = api.users.getAll.useQuery();
-  console.log("data is: ", data);
 
   if (!data) return null;
 
